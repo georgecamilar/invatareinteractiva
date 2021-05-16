75549a0ff35d4a23562791bc4e385e99e153a567
George <georgecamilar2@gmail.com>
2021-05-16 14:09:07 +0300

    * Commit with login and register

--- src/main/java/app/controllers/MappingController.java
+++ src/main/java/app/controllers/MappingController.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiPredicate;
 import java.util.stream.StreamSupport;
 
@@ -17,7 +20,7 @@ import java.util.stream.StreamSupport;
 @RequestMapping(path = "/app")
 public class MappingController {
     private static final Logger logger = LoggerFactory.getLogger(MappingController.class);
-
+    private final List<User> loggedIn = new ArrayList<>();
     @Autowired
     private Services services;
 
@@ -34,7 +37,7 @@ public class MappingController {
     @RequestMapping(path = "/user/config/{id}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<?> getUser(@PathVariable String id) {
         try {
-            Integer givenId = Integer.parseInt(id);
+            final Integer givenId = Integer.parseInt(id);
             return new ResponseEntity<>(services.get(givenId), HttpStatus.OK);
         } catch (NumberFormatException ex) {
             logger.error(ex.getMessage());
@@ -45,11 +48,39 @@ public class MappingController {
 
     @RequestMapping(path = "/user", method = RequestMethod.POST)
     public ResponseEntity<?> login(@RequestBody User user) {
+        if (loggedIn.contains(user)) {
+            return new ResponseEntity<>("Logged in already", HttpStatus.PERMANENT_REDIRECT);
+        }
         if (StreamSupport.stream(services.getAll().spliterator(), false)
                 .filter(el -> userComparator.test(el, user))
                 .count() == 1)
             return ResponseEntity.status(200).build();
         return ResponseEntity.status(403).build();
+    }
+
+    @RequestMapping(path = "/user/register", method = RequestMethod.POST)
+    public ResponseEntity<?> register(@RequestBody Map<String, Object> body) {
+        try {
+            if (body.containsKey("username") && body.containsKey("password")) {
+                final String username = body.get("username").toString();
+                final String password = body.get("password").toString();
+                final long count = StreamSupport.stream(services.getAll().spliterator(), false)
+                        .filter(el -> el.getUsername().equals(username))
+                        .filter(el -> el.getPassword().equals(password))
+                        .count();
+                if (count > 0) {
+                    return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+                }
+                final User user = new User();
+                user.setUsername(username);
+                user.setPassword(password);
+                services.add(user);
+                return new ResponseEntity<>(HttpStatus.PERMANENT_REDIRECT);
+            }
+            return new ResponseEntity<>("Cannot register this user", HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ex) {
+            return new ResponseEntity<>("Inserted information is wrong", HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 
     private final BiPredicate<User, User> userComparator = (streamElement, comparator) ->
